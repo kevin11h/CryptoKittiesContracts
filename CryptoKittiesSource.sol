6adac8ebf19e7999292905d7188be4d341e6a910
Kevin Hung <kevhung11@gmail.com>
2018-08-25 17:50:14 -0700

    Update CryptoKittiesSource.sol

--- CryptoKittiesSource.sol
+++ CryptoKittiesSource.sol
@@ -1,6 +1,11 @@
-// reference: http://ethfiddle.com/09YbyJRfiI
+// insired by article found on medium: https://medium.com/loom-network/how-to-code-your-own-cryptokitties-style-game-on-ethereum-7c8ac86a4eb3
+// paraphrased reference: http://ethfiddle.com/09YbyJRfiI
 // http://etherscan.io/address/0x06012c8cf97bead5deae237070f9587f8e7a266d#code
 
 pragma solidity ^0.4.11;
 
-
+/**
+ *
+ * @title Ownable
+ * @dev The Ownable contract has owner address, and provides basic 
+ * functions, this simplifies 
